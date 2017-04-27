b9d755d8d3a2a7168ebd53d977abaf1784fbe964
Sehi L'Yi <shlyi@hcil.snu.ac.kr>
2017-04-27 18:30:28 +0900

    sample video addition code

--- popup.js
+++ popup.js
@@ -1,6 +1,22 @@
 function onVideoWatch() {
-  var id = "google_companion_ad_div";
-  
+  //code that should be inserted in the below function
+  var id = 'google_companion_ad_div';
+  var rootDiv = document.getElementById(id);
+  var div = document.createElement('div');
+  var url = 'FEsFXsPCVOo';
+  var videoUrl = 'https://www.youtube.com/embed/' + url + '?rel=0&amp;controls=1&autoplay=1&amp;showinfo=1?ecver=1';
+
+  //video
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('width', '300');
+  iframe.setAttribute('height', '169');
+  iframe.setAttribute('src', videoUrl);
+  iframe.setAttribute('frameborder', '0');
+
+  div.appendChild(iframe);
+  rootDiv.appendChild(div);
+  //end of code
+
   var codeStr = "";
 
   chrome.tabs.executeScript(null, {
